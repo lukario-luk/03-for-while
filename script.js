c6202ac408ad2a9912eabeafb71139f248d312f7
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-01-23 21:48:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+  for(let i = 1; i <= 10; i++) {
+    console.log(i);
+} 
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+ let i2 = 1 
+while (i2 <= 10){
+     console.log(i2)
+     i2++;
+ }  
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100 
+for(let i3 = 1; i3 <= 100; i3++) {
+    if (i3 > 0 && i3%2){
+    console.log(i3);
+   }
+}  
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+  for(let i4 = 1; i4 <= 100; i4++) {
+    if (i4%2 == 0){
+     console.log(i4);
+    }
+ } 
